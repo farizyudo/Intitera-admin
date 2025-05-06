16b63c2d796e3ddb184f61421b195dc0653a8528
farizyudo <farizyudop.02@gmail.com>
2025-05-06 14:03:12 +0700

    Setup Cypress

---
+++ cypress/e2e/01-login.cy.js
@@ -0,0 +1,13 @@
+
+
+
+describe('Login Intitera', () => {
+
+  beforeEach(() => {
+    cy.visit('https://terafarma-fe.geekgarden.my.id/')
+  })
+
+  it('Login Success', () => {
+    cy.visit('https://terafarma-fe.geekgarden.my.id/')
+  })
+})
---
+++ cypress/support/loginElement.js
@@ -0,0 +1,23 @@
+class loginElement {
+    email = ':nth-child(2) > .chakra-input'
+
+    pass = ':nth-child(3) > .chakra-input'
+    logInButton = '.chakra-button'
+
+    inputUsername(userEmail) {
+        cy.get(this.email)
+            .clear()
+            .type(userEmail)
+    }
+
+    inputPassword(userPass) {
+        cy.get(this.pass)
+            .clear()
+            .type(userPass)
+    }
+
+    BTNlogin() {
+        cy.get(this.logInButton).click()
+    }
+}
+export default new loginElement()
